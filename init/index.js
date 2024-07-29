308b568ee6cd290dd41e64c800275073d3b6f53e
zht-git-0 <zht2226070364@163.com>
2024-07-29 14:55:41 +0800

    1

--- init/index.js
+++ init/index.js
@@ -1,2 +1,17 @@
 let side = document.getElementById('side')
 side.style.display = 'none'
+let memu_button = document.getElementById('btn-side')
+memu_button.addEventListener('click', function() {
+    if (side.style.display === 'none') {
+        side.style.display = 'block'
+        memu_button.style.display = 'none'
+    }
+})
+let close_button = document.getElementById('close-menu')
+close_button.addEventListener('click', function() {
+    if (side.style.display !== 'none'){
+        side.style.display = 'none'
+        memu_button.style.display = 'block'
+    }
+})
+//side.style.display = 'none'
